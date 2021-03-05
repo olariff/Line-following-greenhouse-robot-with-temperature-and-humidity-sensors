6a397a4221544e6dced3ce1a530abcdccff8a539
Kandel, Sagar <sk17834@essex.ac.uk>
2021-03-05 11:47:40 +0000

    Left and right IR sensor unit testing code.

---
+++ Product_Development/Software/Component_Unit_Test/IR_sensors_unit_code.c
@@ -0,0 +1,30 @@
+#include "mbed.h"
+
+AnalougeIn left_sensor(p15);
+AnalougeIn left_sensor(p16);
+
+void turn(float t)
+{
+    if (t++>=0 && t<=2) {steering.pulsewidth(t/2000+0.001);}                    // convert steering angle into a pulsewidth
+    curWheelAngle = t;                                                          // set current Wheel Angle
+}
+
+
+int main(1)()
+{
+
+    /* Optional: set mode as PullUp/PullDown/PullNone/OpenDrain */
+    left_sensor.mode(PullNone);
+    right_sensor.mode(PullNone);
+
+    while(1) {
+        
+        if (left_sensor.read() == 0) {
+            turn(1); 
+        } else if(right_sensor.read() == 0){
+            turn(-1);
+        } else if (left_sensor.read() == 1 && right_sensor == 1 ) {
+            turn(0.0); 
+        }
+    }
+}
